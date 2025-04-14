92e4f3786014b9af033f255f4984fb89d93229ba
rakesh15092002 <140505827+rakesh15092002@users.noreply.github.com>
2025-04-14 09:53:26 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,22 +1,14 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import { GridFSBucket } from 'mongodb';
 dotenv.config({});
+
 
 export const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}`);
-        console.log("Database connected successfully");
-
-        // Initialize GridFSBucket
-        const db = mongoose.connection.db;
-        const bucket = new GridFSBucket(db, { bucketName: 'uploads' });
-        console.log("GridFSBucket initialized successfully");
-
-        // Now you can use `bucket` to store/retrieve files from GridFS
-        return bucket;
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}`)
+        console.log("DataBase connected successfully");
     } catch (error) {
         console.log("MONGODB connection FAILED ", error);
-        process.exit(1);
+        process.exit(1)
     }
-};
+}
